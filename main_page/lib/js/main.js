978345fed5a527b8cf1a1be4babed32c3f5ae2f9
Sun Dai <sundaisf@gmail.com>
2014-11-28 11:56:04 -0800

    clanup

--- main_page/lib/js/main.js
+++
@@ -1,6 +0,0 @@
-// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
