136b424171362189afccdb79ff780369a71c5778
Sun Dai <sundaisf@gmail.com>
2014-12-07 20:35:12 -0800

    tweaked scrolling attributes, fixed navigation problems, added enquire.js in order to make page responsive

--- fog/lib/js/main.js
+++ fog/lib/js/main.js
@@ -1,6 +1,54 @@
-// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+//fix responsiveness
+function adjustWindow(){
+     
+    // Get window size
+    winH = $window.height();
+    winW = $window.width();
+     
+    // Keep minimum height 550
+    if(winH <= 550) {
+        winH = 550;
+    }
+     
+    // Init Skrollr for 768 and up
+    if( winW >= 768) {
+ 
+        // Init Skrollr
+        var s = skrollr.init({
+            forceHeight: false
+        });
+ 
+        // Resize our slides
+        $slide.height(winH);
+ 
+        s.refresh($('.homeSlide'));
+ 
+    } else {
+ 
+        // Init Skrollr
+        var s = skrollr.init();
+        s.destroy();
+    }
+ 
+    // Check for touch
+    if(Modernizr.touch) {
+ 
+        // Init Skrollr
+        var s = skrollr.init();
+        s.destroy();
+    }
+ 
+}
+
+function initAdjustWindow() {
+    return {
+        match : function() {
+            adjustWindow();
+        },
+        unmatch : function() {
+            adjustWindow();
+        }
+    };
+}
+ 
+enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false);
